5835ad94701340abdda0f79a4400da17b00282b7
Bob Weigel <rweigel@gmu.edu>
2022-05-23 11:15:09 -0400

    add cache and imgs

--- misc/availability.py
+++ misc/availability.py
@@ -1,4 +1,10 @@
+import sys
+import logging
+
 from hapiclient import hapitime2datetime
+
+# Change INFO to WARNING or ERROR to suppress logging messages in this script
+logging.basicConfig(level=logging.INFO)
 
 from util import availability
 
@@ -9,22 +15,39 @@ ids = []
 starts = []
 stops = []
 n_max = len(datasets)
-#n_max = 50
+#n_max = 50 # Plot only first n_max datasets
+
+# Solution to Working with Metadata I - Basic - 1.
 
 # Create table
 n = 0
 datasets.reverse()
+table = []
 for idx, dataset in enumerate(datasets):
     # Pad ids. Assumes max id length is 14 chars.
     id = datasets[idx]["id"]
     idp = '{0:15s}'.format(id)
-    print(f'{idp}  {datasets[idx]["startDate"]}  {datasets[idx]["stopDate"]}')
+    line = f'{idp}  {datasets[idx]["startDate"]}  {datasets[idx]["stopDate"]}'
+    print(line)
+    table.append(line)
+
     ids.append(id)
     starts.append(hapitime2datetime(datasets[idx]["startDate"])[0])
     stops.append(hapitime2datetime(datasets[idx]["stopDate"])[0])
     n = n + 1
     if n > n_max:
         break
+
+sys.stdout.flush()
+
+from datetime import datetime
+logging.info("Writing availability.txt")
+with open('availability.txt', 'w', encoding = 'utf-8') as f:
+    f.write("Table created on " + datetime.now().isoformat() + "\n\n")
+    f.write("\n".join(table))
+logging.info("Wrote   availability.txt")
+
+# Solution to Working with Metadata I - Basic - 2.
 
 # Plot
 import matplotlib.pyplot as plt
@@ -34,7 +57,7 @@ def newfig():
     fig, ax = plt.subplots()
     fig.set_figheight(10)
     fig.set_figwidth(8)
-    return fig, ax
+    return ax
 
 def figconfig(ax):
     ax.spines['top'].set_visible(False)
@@ -47,7 +70,8 @@ def figconfig(ax):
     datetick('x')
 
 idx = []
-fix, ax = newfig()
+ax = newfig()
+fn = 1
 for n in range(len(ids)):
     line, = ax.plot([starts[n], stops[n]], [n, n], linewidth=5)
     lc = line.get_color()
@@ -55,8 +79,19 @@ for n in range(len(ids)):
     ax.text(stops[n], n, ' ' + ids[n], color=lc, verticalalignment='center')
     if (n + 1) % 50 == 0:
         figconfig(ax)
-        fix, ax = newfig()
+        plt.tight_layout()
+        fname = f"./availability-{fn}.svg"
+        logging.info(f'Writing {fname}')
+        plt.savefig(fname, bbox_inches='tight')
+        logging.info(f'Wrote   {fname}')
+        fn = fn + 1
+        ax = newfig()
 
 # Finish last plot, if needed
 if (n + 1) % 50 != 0:
     figconfig(ax)
+    plt.tight_layout()
+    logging.info(f'Writing {fname}')
+    plt.savefig(f"./availability-{fn}.svg", bbox_inches='tight')
+    logging.info(f'Wrote   {fname}')
+
--- misc/util.py
+++ misc/util.py
@@ -7,32 +7,37 @@ from hapiclient import hapi
 # Change INFO to WARNING or ERROR to suppress logging messages in this script
 logging.basicConfig(level=logging.INFO)
 
+# Solution to working with Metadata II.
+
 def availability(server):
 
     if os.path.exists("availability.pkl"):
         with open('availability.pkl', 'rb') as f:
+            logging.info('Reading availability.pkl')
             datasets = pickle.load(f)
-            logging.info('Read availability.pkl')
-    else:
-        resp = hapi(server)
-        logging.info(resp)
+            logging.info('Read    availability.pkl')
 
-        datasets = resp['catalog']
-        logging.info(datasets)
+        return datasets
 
-        for idx, dataset in enumerate(datasets):
-            logging.info(f'Working on dataset id {datasets[idx]}')
-            resp = hapi(server, dataset["id"], logging=True)
-            startDate = resp["startDate"]
-            stopDate = resp["stopDate"]
-            # Add start/stop to each element in datasets list
-            datasets[idx]["startDate"] = startDate
-            datasets[idx]["stopDate"] = stopDate
-            logging.info(f'  start = {startDate}\tstop = {stopDate}')
+    resp = hapi(server)
+    logging.info(resp)
 
-        # Save result so we don't need to recreate when we modify table and plot code.
-        with open('availability.pkl', 'wb') as f:
-            pickle.dump(datasets, f)
-            logging.info('Saved availability.pkl')
+    datasets = resp['catalog']
+    logging.info(datasets)
+
+    for idx, dataset in enumerate(datasets):
+        logging.info(f'Working on dataset id {datasets[idx]}')
+        resp = hapi(server, dataset["id"], logging=True)
+        startDate = resp["startDate"]
+        stopDate = resp["stopDate"]
+        # Add start/stop to each element in datasets list
+        datasets[idx]["startDate"] = startDate
+        datasets[idx]["stopDate"] = stopDate
+
+    # Save result so we don't need to recreate when we modify table and plot code.
+    with open('availability.pkl', 'wb') as f:
+        logging.info('Saving availability.pkl')
+        pickle.dump(datasets, f)
+        logging.info('Saved  availability.pkl')
 
     return datasets
