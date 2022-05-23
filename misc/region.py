011d29c38362f075a6b26daeb3df8f306cc44c65
Bob Weigel <rweigel@gmu.edu>
2022-05-23 12:13:51 -0400

    region.txt

--- misc/region.py
+++ misc/region.py
@@ -1,9 +1,12 @@
+import sys
 import logging
 
 from hapiclient import hapi
 from hapiclient import hapitime2datetime
 
 from util import availability
+
+# Solution for Working with Metadata II
 
 short_run = True # If True, only get data for first three s/c
 
@@ -25,10 +28,9 @@ for idx, dataset in enumerate(datasets):
     stop_available = hapitime2datetime(datasets[idx]["stopDate"])[0]
 
     if start_available <= start_wanted and stop_available >= stop_wanted:
-        print(f'{id}  {datasets[idx]["startDate"]}  {datasets[idx]["stopDate"]}')
         n = n+1
 
-        logging.info(f'Getting data for {datasets[idx]}')
+        logging.info(f'Getting data for {datasets[idx]["id"]} from {datasets[idx]["startDate"]} to {datasets[idx]["stopDate"]}')
         data, meta = hapi(server, datasets[idx]["id"], 'Spacecraft_Region', start, stop, logging=False)
 
         if len(data['Spacecraft_Region']) > 0:
@@ -41,22 +43,37 @@ for idx, dataset in enumerate(datasets):
         break
 
 
-print(f'\n{n} S/C have ephemeris data from {start} to {stop}')
+if short_run:
+    print(f'\nAvailability of first 4 S/C from {start} to {stop}')
+else:
+    print(f'\n{n} S/C have ephemeris data from {start} to {stop}')
+
 print(80*"-")
-print(f"Spacecraft region for first available value between {start} to {stop}")
+print("S/C ID           Region      Time")
 print("")
 n = 0
+table = []
 for idx, dataset in enumerate(datasets):
     id = "{:15s}".format(datasets[idx]["id"])
     if "Spacecraft_Region" in datasets[idx]:
         n = n + 1
         if datasets[idx]["Spacecraft_Region"] is not None:
-            print(f'{id}  {datasets[idx]["Spacecraft_Region"]}\t {datasets[idx]["First_Value"]}')
+            line = f'{id}  {datasets[idx]["Spacecraft_Region"]}\t {datasets[idx]["First_Value"]}'
         else:
-            print(f'{id}  No values available')
+            line = f'{id}  No values available'
+        print(line)
+        table.append(line)
 
     if short_run and n > 3:
         break
 print(80*"-")
 
+sys.stdout.flush()
+
+from datetime import datetime
+logging.info("Writing region.txt")
+with open('region.txt', 'w', encoding = 'utf-8') as f:
+    f.write("Table created on " + datetime.now().isoformat() + "\n\n")
+    f.write("\n".join(table))
+logging.info("Wrote   region.txt")
 
