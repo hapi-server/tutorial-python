cc3fb213a853c1f9c5bff2e6fb7e6ed584762d7b
Bob Weigel <rweigel@gmu.edu>
2022-06-14 20:36:00 -0400

    post summer school updates

--- misc/Problem02b.py
+++
@@ -1,8 +0,0 @@
-print(df)
-
-print("Means")
-print(df.mean())
-print("\nStandard Deviations")
-print(df.std())
-
-print(f"\nMax value of scalar: {df['scalar'].max()} at time {df['scalar'].idxmax()}")
